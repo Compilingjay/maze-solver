e7de4ce0f9c802d95368eee8ae146717f776a499
Compiledjay <139014932+Compilingjay@users.noreply.github.com>
2024-05-30 10:43:14 -0700

    feat: add cells and display in main

---
+++ cell.py
@@ -0,0 +1,71 @@
+from window import (
+    Window,
+)
+
+from drawable import (
+    Point,
+    Line
+)
+
+
+class Cell():
+    def __init__(self, win: Window) -> None:
+        self.has_left:   bool = True
+        self.has_right:  bool = True
+        self.has_top:    bool = True
+        self.has_bottom: bool = True
+        self._x1: int = None
+        self._y1: int = None
+        self._x2: int = None
+        self._y2: int = None
+        self._win = win
+    
+    def draw(self, x1: int, y1: int, x2: int, y2: int) -> None:
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+
+        if self.has_left:
+            left_wall = Line(
+                Point(self._x1, self._y1),
+                Point(self._x1, self._y2)
+                )
+            self._win.draw_line(left_wall)
+        if self.has_right:
+            right_wall = Line(
+                Point(self._x2, self._y1),
+                Point(self._x2, self._y2)
+                )
+            self._win.draw_line(right_wall)
+        if self.has_top:
+            top_wall = Line(
+                Point(self._x1, self._y1),
+                Point(self._x2, self._y1)
+                )
+            self._win.draw_line(top_wall)        
+        if self.has_bottom:
+            bottom_wall = Line(
+                Point(self._x1, self._y2),
+                Point(self._x2, self._y2)
+                )
+            self._win.draw_line(bottom_wall)
+    
+
+    def draw_move(self, to_cell: object, undo=False):
+        path_color: str = ""
+        if undo:
+            path_color = "red"
+        else:
+            path_color = "gray"
+        
+        path = Line(
+            Point(
+                (self._x1 + self._x2) / 2,
+                (self._y1 + self._y2) / 2),   
+            Point(
+                (to_cell._x1 + to_cell._x2) / 2,
+                (to_cell._y1 + to_cell._y2) / 2)
+            )
+        
+        self._win.draw_line(path, path_color)
--- main.py
+++ main.py
@@ -18,6 +18,9 @@ def main():
     p2 = Point(400, 500)
     line = Line(p1, p2)
     win.draw_line(line, "black")
+    cell = Cell(win)
+    cell._win = win
+    cell.draw(p1.x, p1.y, p2.x, p2.y)
     win.wait_for_close()
 
 
